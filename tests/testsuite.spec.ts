da833a6e31ab25eeb1e82e27da6ca61a22b0899b
Lana <lanasaeed@hotmail.com>
2024-09-30 13:25:25 +0200

    Get all rooms

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,18 +1,37 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, APIRequestContext } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const BASE_URL = 'http://localhost:3000';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('TheTester Hotel API Tests', () => {
+  let request: APIRequestContext;
+  let token: string;
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test.beforeAll(async ({ playwright }) => {
+    request = await playwright.request.newContext({
+      baseURL: BASE_URL,
+    });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+    const loginResponse = await request.post('/api/login', {
+      data: {
+        username: 'tester01',
+        password: 'GteteqbQQgSr88SwNExUQv2ydb7xuf8c',
+      },
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const loginData = await loginResponse.json();
+    token = loginData.token;
+  });
+
+  // 1. Get All Rooms
+  test('Test case 01 - Get all rooms', async () => {
+    const response = await request.get('/api/rooms', {
+      headers: { 'X-user-auth': JSON.stringify({ username: 'tester01', token }) },
+    });
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    const rooms = await response.json();
+    expect(rooms.length).toBeGreaterThan(0);
+  });
+})
