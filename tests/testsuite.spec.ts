a110a08e39473ef1cfc63ff35525f3b3f2fdf453
Alexandros020 <148055035+Alexandros020@users.noreply.github.com>
2024-09-30 14:23:03 +0200

    update client

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,18 +1,47 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, APIRequestContext } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const BASE_URL = 'http://localhost:3000';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('TheTester Hotel API Tests', () => {
+  let request: APIRequestContext;
+  let token: string;
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test.beforeAll(async ({ playwright }) => {
+    request = await playwright.request.newContext({
+      baseURL: BASE_URL,
+    });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+    const loginResponse = await request.post('/api/login', {
+      data: {
+        username: 'tester01',
+        password: 'GteteqbQQgSr88SwNExUQv2ydb7xuf8c',
+      },
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const loginData = await loginResponse.json();
+    token = loginData.token;
+  });
+
+  // 6. Update Client
+  test('Test case 06 - Update a client', async () => {
+    const clientId = 1;
+    const payload = {
+      id: '1',
+      name: 'Stina Johnson',
+      email: 'stina.johnson@example.com',
+    };
+    const response = await request.put(`/api/client/${clientId}`, {
+      data: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-user-auth': JSON.stringify({ username: 'tester01', token }),
+      },
+    });
+    expect(response.ok()).toBeTruthy();
+    const updatedClient = await response.json();
+    expect(updatedClient.name).toBe(payload.name);
+  });
+
+})
